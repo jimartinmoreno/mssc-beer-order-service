dbe8a3657489db0841bc5af13b1c735e257ade82
Nacho Martin <joseignacio.martinmoreno@gmail.com>
2021-12-02 13:35:26 +0100

    Revision de código para usar colas ActiveMQ artemis

--- src/test/java/guru/sfg/beer/order/service/services/BeerOrderManagerImplIT.java
+++ src/test/java/guru/sfg/beer/order/service/services/BeerOrderManagerImplIT.java
@@ -43,7 +43,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 @ExtendWith(WireMockExtension.class)
 @SpringBootTest
-public class BeerOrderManagerImplIT {
+class BeerOrderManagerImplIT {
 
     @Autowired
     BeerOrderManager beerOrderManager;
